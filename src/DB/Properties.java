cc94297a7332016e63f25f66a1d62901188c8dca
Manuka Yasas <63389716+manuka99@users.noreply.github.com>
2021-04-17 00:14:09 +0530

    Delete src directory

--- src/DB/Properties.java
+++
@@ -1,8 +0,0 @@
-package DB;
-
-public class Properties {
-	public static String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	public static String URL = "jdbc:mysql://localhost:3306/testingdb";
-	public static String USERNAME = "root";
-	public static String PASSWORD = "";
-}
